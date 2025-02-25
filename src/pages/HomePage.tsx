78e78a3f148917b931cce1489f4739bafeab768b
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-02-25 07:46:49 +0000

    changing App.tsx to a routing library

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,31 @@
+import '/styles/styles.css'
+
+
+function HomePage() {
+    return (
+      <>
+       <h1>Road to Master</h1>
+       <div className="body">
+       <div className="sidebar">
+        <h2>Projects</h2>
+        <ul>
+        <li>Coding</li>
+        <li>Warhammer</li>
+        <li>Gaming</li>
+       </ul>
+       </div>
+  
+       <div className="content">
+        <h2>My Coding Journey</h2>
+        <div className="daily-log">
+        <p>
+          I've been coding for a yeara now. I started with HTML and CSS, then moved on to JavaScript. I am also starting to learn React and using it to build website. Like this one.
+        </p>
+        </div>
+       </div>
+       </div>
+      </>
+    )
+  }
+  
+  export default HomePage
